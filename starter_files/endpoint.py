e372673cf86b5c339a06a560f5187b881a2f47f5
Graflinger <39885965+Graflinger@users.noreply.github.com>
2021-06-03 21:03:58 +0200

    final

--- starter_files/endpoint.py
+++ starter_files/endpoint.py
@@ -1,73 +1,85 @@
-import requests
+import urllib.request
 import json
+import os
+import ssl
 
-# URL for the web service, should be similar to:
-# 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
-scoring_uri = ''
-# If the service is authenticated, set the key or token
-key = ''
 
-# Two sets of data to score, so we get two results back
+def allowSelfSignedHttps(allowed):
+    # bypass the server certificate verification on client side
+    if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
+        ssl._create_default_https_context = ssl._create_unverified_context
+
+
+# this line is needed if you use self-signed certificate in your scoring service.
+allowSelfSignedHttps(True)
+
 data = {"data":
         [
-          {
-            "age": 17,
-            "campaign": 1,
-            "cons.conf.idx": -46.2,
-            "cons.price.idx": 92.893,
-            "contact": "cellular",
-            "day_of_week": "mon",
-            "default": "no",
-            "duration": 971,
-            "education": "university.degree",
-            "emp.var.rate": -1.8,
-            "euribor3m": 1.299,
-            "housing": "yes",
-            "job": "blue-collar",
-            "loan": "yes",
-            "marital": "married",
-            "month": "may",
-            "nr.employed": 5099.1,
-            "pdays": 999,
-            "poutcome": "failure",
-            "previous": 1
-          },
-          {
-            "age": 87,
-            "campaign": 1,
-            "cons.conf.idx": -46.2,
-            "cons.price.idx": 92.893,
-            "contact": "cellular",
-            "day_of_week": "mon",
-            "default": "no",
-            "duration": 471,
-            "education": "university.degree",
-            "emp.var.rate": -1.8,
-            "euribor3m": 1.299,
-            "housing": "yes",
-            "job": "blue-collar",
-            "loan": "yes",
-            "marital": "married",
-            "month": "may",
-            "nr.employed": 5099.1,
-            "pdays": 999,
-            "poutcome": "failure",
-            "previous": 1
-          },
-      ]
-    }
-# Convert to JSON string
-input_data = json.dumps(data)
-with open("data.json", "w") as _f:
-    _f.write(input_data)
+            {
+                'age': "17",
+                'job': "blue-collar",
+                'marital': "married",
+                'education': "example_value",
+                'default': "no",
+                'housing': "yes",
+                'loan': "yes",
+                'contact': "cellular",
+                'month': "may",
+                'day_of_week': "mon",
+                'duration': "971",
+                'campaign': "1",
+                'pdays': "999",
+                'previous': "1",
+                'poutcome': "failure",
+                'emp.var.rate': "-1.8",
+                'cons.price.idx': "92.893",
+                'cons.conf.idx': "-46.2",
+                'euribor3m': "1.299",
+                'nr.employed': "5099.1",
+            },
+            {
+                'age': "87",
+                'job': "blue-collar",
+                'marital': "married",
+                'education': "university.degree",
+                'default': "no",
+                'housing': "yes",
+                'loan': "yes",
+                'contact': "cellular",
+                'month': "may",
+                'day_of_week': "mon",
+                'duration': "471",
+                'campaign': "1",
+                'pdays': "999",
+                'previous': "1",
+                'poutcome': "failure",
+                'emp.var.rate': "-1.8",
+                'cons.price.idx': "92.893",
+                'cons.conf.idx': "-46.2",
+                'euribor3m': "1.299",
+                'nr.employed': "5099.1",
+            },
+        ]
+        }
 
-# Set the content type
-headers = {'Content-Type': 'application/json'}
-# If authentication is enabled, set the authorization header
-headers['Authorization'] = f'Bearer {key}'
+body = str.encode(json.dumps(data))
 
-# Make the request and display the response
-resp = requests.post(scoring_uri, input_data, headers=headers)
-print(resp.json())
+url = 'http://2eab34d1-a898-40f3-87b8-c0bbeff49db5.southcentralus.azurecontainer.io/score'
+# Replace this with the API key for the web service
+api_key = '8PGuiQlod3wCMKVS0fFwyK2hIpLdAHqc'
+headers = {'Content-Type': 'application/json',
+           'Authorization': ('Bearer ' + api_key)}
 
+req = urllib.request.Request(url, body, headers)
 
+try:
+    response = urllib.request.urlopen(req)
+
+    result = response.read()
+    print(result)
+except urllib.error.HTTPError as error:
+    print("The request failed with status code: " + str(error.code))
+
+    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+    print(error.info())
+    print(json.loads(error.read().decode("utf8", 'ignore')))
